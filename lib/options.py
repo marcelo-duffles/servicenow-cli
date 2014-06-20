f0f825321afa69851ff6de43829731f65601c5f6
Marcelo D. D. Moreira <marceloddm@gmail.com>
2014-06-19 21:30:32 -0300

    refactoring: custom method to parse parameters

--- lib/options.py
+++ lib/options.py
@@ -12,6 +12,15 @@ except Exception, e:
 
 import soap_api
 
+def parse_parameters(parameters):
+    data = {
+        'cmdb_ci': '.'.join(parameters.split(';')[0].split('.')[0:2]),
+        'u_opm_severity': parameters.split(';')[1],
+        'u_opm_entity': parameters.split(';')[2],
+        'short_description': ''.join(parameters.split(';')[3:])
+    }
+    return data
+
 
 def handle_get_incident(incident_number):
     data = {
@@ -38,26 +47,12 @@ def handle_get_ci(ci_name):
 
 
 def handle_insert_incident(parameters):
-
-    data = {
-        'cmdb_ci': '.'.join(parameters.split(';')[0].split('.')[0:2]),
-        'u_opm_severity': parameters.split(';')[1],
-        'u_opm_entity': parameters.split(';')[2],
-        'short_description': ''.join(parameters.split(';')[3:])
-    }
-    response = soap_api.insert('incident', data)
+    response = soap_api.insert('incident', parse_parameters(parameters))
     print response
 
 
 def handle_insert_alert_event(parameters):
-
-    data = {
-        'cmdb_ci': '.'.join(parameters.split(';')[0].split('.')[0:2]),
-        'u_opm_severity': parameters.split(';')[1],
-        'u_opm_entity': parameters.split(';')[2],
-        'short_description': ''.join(parameters.split(';')[3:])
-    }
-    response = soap_api.insert('u_alert_event', data)
+    response = soap_api.insert('u_alert_event', parse_parameters(parameters))
     print response
 
 
